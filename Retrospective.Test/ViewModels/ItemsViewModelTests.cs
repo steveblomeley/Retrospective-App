42de0758a602dbc8eb7912a1e076e0feff4c1aeb
steveblomeley <steveblomeley@yahoo.co.uk>
2018-06-20 22:44:42 +0000

    Add unit tests for the ItemsViewModel

---
+++ Retrospective.Test/ViewModels/ItemsViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Moq;
+using NUnit.Framework;
+using Retrospective.Models;
+using Retrospective.Navigation;
+using Retrospective.ViewModels;
+
+namespace Retrospective.Test.ViewModels
+{
+    [TestFixture]
+    public class ItemsViewModelTests
+    {
+        private Mock<IAppNavigation> _appNavigation;
+        private ItemsViewModel _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _appNavigation = new Mock<IAppNavigation>();
+            _sut = new ItemsViewModel(_appNavigation.Object);
+        }
+
+        [Test]
+        public void ItemsProperty_DoesNotContainAnyItemsInitially()
+        {
+            Assert.That(_sut.Items, Is.Empty);
+        }
+
+        [Test]
+        public void ItemsProperty_IsAnObservableCollection()
+        {
+            Assert.That(_sut.Items, Is.InstanceOf<ObservableCollection<Item>>());
+        }
+
+        [Test]
+        public void AddNewItemCommand_NavigatesToTheAddNewItemPage_PassingInAReferenceToTheAddMethodOnTheItemsCollection()
+        {
+            _sut.AddNewItemCommand.Execute(null);
+
+            _appNavigation.Verify(x => x.AddNewItem(It.IsAny<Action<Item>>()), Times.Once);
+            _appNavigation.Verify(x => x.AddNewItem(_sut.Items.Add));
+        }
+    }
+}
