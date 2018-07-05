dc2ebefabb86dc5e1d71c4da4ccee70602ae5f3f
steve <steveblomeley@yahoo.co.uk>
2018-07-05 09:31:37 +0000

    Wire in MvvmCross packages
    Wire in MvvmX nuget packages
    Add Core project and App class
    Hook up Android MainActivity and iOS AppDelegate to use MvvmX

---
+++ Retrospective.Core/App.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.ViewModels;
+
+namespace Retrospective.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
--- Retrospective/Retrospective.Android/MainActivity.cs
+++ Retrospective/Retrospective.Android/MainActivity.cs
@@ -1,27 +1,12 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Platforms.Android.Views;
 
 namespace Retrospective.Droid
 {
     [Activity(Label = "Retrospective", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<Core.App, App>, Core.App, App>
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(bundle);
-
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
     }
 }
-
